fb9dd94d3e025e7818b97c3d1d445217a2416358
EC2 Default User <ec2-user@ip-172-31-32-76.ec2.internal>
2019-09-26 21:30:24 +0000

    Second Commit

---
+++ build/precache-manifest.0e72f465ffa60f2e7e78e8b151334cde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39f752a7a2ca0607e9494886549f45fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa4041681c35b6da5b66",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0b29977d5d94cb6cf3cc",
+    "url": "/static/js/2.e32a7d0e.chunk.js"
+  },
+  {
+    "revision": "aa4041681c35b6da5b66",
+    "url": "/static/js/main.895d34d9.chunk.js"
+  },
+  {
+    "revision": "01439101267f78f50584",
+    "url": "/static/js/runtime-main.a49cfc28.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
